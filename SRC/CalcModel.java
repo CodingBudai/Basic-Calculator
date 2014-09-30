0be2db276c097035789df39046c1542c0ed55bbf
Adrian Cleofe <rastaman8693@gmail.com>
2014-09-30 11:16:23 +0800

    Create CalcModel.java

---
+++ SRC/CalcModel.java
@@ -0,0 +1,28 @@
+
+public class CalcModel {
+	private String tempFirstValue = "";
+	private boolean dispIsEmpty;
+	
+	
+	public boolean dispIsEmpty(){
+		if(this.tempFirstValue.equals("")){
+			dispIsEmpty = true;
+			return dispIsEmpty;
+		}
+		else{
+			dispIsEmpty = false;
+			return dispIsEmpty;
+		}
+	}
+	
+	public void setTempFirstValue(String txt){
+		if(this.dispIsEmpty == true)
+			this.tempFirstValue = txt;
+		else
+			this.tempFirstValue += txt;
+	}
+	
+	public String getTempFirstValue(){
+		return this.tempFirstValue;
+	}
+}
