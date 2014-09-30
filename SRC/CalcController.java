6fe5b46a772bf9bcde6a76586de26cc9e040a7f2
Adrian Cleofe <rastaman8693@gmail.com>
2014-09-30 20:35:17 +0800

    Update CalcController.java

--- SRC/CalcController.java
+++ SRC/CalcController.java
@@ -9,7 +9,7 @@ public class CalcController {
 		this.theView = theView;
 		this.theModel = theModel;
 		
-		this.theModel.setTempFirstValue(this.theView.getCurrentTxt());
+		//this.theModel.setTempFirstValue(this.theView.getCurrentTxt());
 		this.theView.addListener(new CalcListener());
 	}
 	
@@ -21,90 +21,100 @@ public class CalcController {
 					theModel.setTempFirstValue("0");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("0");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn1()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("1");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("1");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn2()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("2");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("2");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn3()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("3");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("3");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn4()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("4");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("4");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn5()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("5");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("5");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn6()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("6");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("6");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn7()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("7");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("7");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn8()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("8");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("8");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 			if(e.getSource() == theView.getNumBtn9()){
 				if(theModel.dispIsEmpty() == true){
 					theModel.setTempFirstValue("9");
 					theView.setDispText(theModel.getTempFirstValue());
 				}
-				else
+				else{
 					theModel.setTempFirstValue("9");
 					theView.setDispText(theModel.getTempFirstValue());
+				}
 			}
 		}
 	}
