52fa6a30203c2926123cb53bb0cf223207b5c9d3
Adrian Cleofe <rastaman8693@gmail.com>
2014-09-30 11:15:00 +0800

    Update CalcMain.java

--- SRC/CalcMain.java
+++ SRC/CalcMain.java
@@ -1,9 +1,10 @@
-
 public class CalcMain {
 
 	public static void main(String[] args) {
 		CalcView view = new CalcView();
-
+		CalcModel model = new CalcModel();
+		CalcController controller = new CalcController(view,model);
+		view.setVisible(true);
 	}
 
 }
