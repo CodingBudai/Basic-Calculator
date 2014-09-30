dd46f142f44d8d9cb821c185a2fc11763bdfdb4b
Adrian Cleofe <rastaman8693@gmail.com>
2014-09-30 20:34:45 +0800

    Update CalcModel.java

--- SRC/CalcModel.java
+++ SRC/CalcModel.java
@@ -1,24 +1,18 @@
 
 public class CalcModel {
 	private String tempFirstValue = "";
-	private boolean dispIsEmpty;
-	
 	
 	public boolean dispIsEmpty(){
-		if(this.tempFirstValue.equals("")){
-			dispIsEmpty = true;
-			return dispIsEmpty;
-		}
-		else{
-			dispIsEmpty = false;
-			return dispIsEmpty;
-		}
+		if(this.tempFirstValue.equals(""))
+			return true;
+		if(this.tempFirstValue.startsWith("0"))
+			return true;
+		else
+			return false;
 	}
 	
 	public void setTempFirstValue(String txt){
-		if(this.dispIsEmpty == true)
-			this.tempFirstValue = txt;
-		if(this.tempFirstValue.startsWith("0"))
+		if(dispIsEmpty() == true)
 			this.tempFirstValue = txt;
 		else
 			this.tempFirstValue += txt;
