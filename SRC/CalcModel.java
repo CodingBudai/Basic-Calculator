5297a20c02b4c83bee84f7f92043c7b06efb616b
Adrian Cleofe <rastaman8693@gmail.com>
2014-10-01 18:31:24 +0800

    Update CalcModel.java

--- SRC/CalcModel.java
+++ SRC/CalcModel.java
@@ -1,6 +1,16 @@
 
 public class CalcModel {
 	private String tempFirstValue = "";
+	private String result = "";
+	
+	private boolean add = false,
+			sub = false,
+			multi = false,
+			div = false;
+	
+	private double	x = 0,
+			y = 0,
+			z = 0;
 	
 	public boolean dispIsEmpty(){
 		if(this.tempFirstValue.equals(""))
@@ -20,5 +30,74 @@ public class CalcModel {
 	
 	public String getTempFirstValue(){
 		return this.tempFirstValue;
+	}
+	
+	public void setAddOperation(boolean thisOperation){
+		this.add = thisOperation;
+		this.x = Double.parseDouble(tempFirstValue);
+		this.tempFirstValue = "";
+	}
+	
+	public void setSubOperation(boolean thisOperation){
+		this.sub = thisOperation;
+		this.x = Double.parseDouble(tempFirstValue);
+		this.tempFirstValue = "";
+	}
+	
+	public void setMultiOperation(boolean thisOperation){
+		this.multi = thisOperation;
+		this.x = Double.parseDouble(tempFirstValue);
+		this.tempFirstValue = "";
+	}
+	
+	public void setDivOperation(boolean thisOperation){
+		this.div = thisOperation;
+		this.x = Double.parseDouble(tempFirstValue);
+		this.tempFirstValue = "";
+	}
+		
+	public void EqualOperation(){
+		if(this.add){
+			this.y = Double.parseDouble(tempFirstValue);
+			this.z = this.x + this.y;
+			this.result = Double.toString(z);
+			this.add = false;
+		}
+		if(this.sub){
+			this.y = Double.parseDouble(tempFirstValue);
+			this.z = this.x - this.y;
+			this.result = Double.toString(z);
+			this.sub = false;
+		}
+		if(this.multi){
+			this.y = Double.parseDouble(tempFirstValue);
+			this.z = this.x * this.y;
+			this.result = Double.toString(z);
+			this.multi = false;
+		}
+		if(this.div){
+			this.y = Double.parseDouble(tempFirstValue);
+			this.z = this.x / this.y;
+			this.result = Double.toString(z);
+			this.div = false;
+		}
+		else
+			this.z = Double.parseDouble(tempFirstValue);
+	}
+	
+	public void ClearAll(){
+		this.add = false;
+		this.sub = false;
+		this.multi = false;
+		this.div = false;
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+		this.tempFirstValue = "";
+		this.result = "";
+	}
+	
+	public String getResult(){
+		return this.result;
 	}
 }
